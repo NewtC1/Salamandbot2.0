44348c999f1e5a144d00801cf451d4cf61bccac0
NewtC <newtc12@gmail.com>
2021-04-11 15:03:02 -0700

    Baseline twitch bot. Also added the logger and argument parser

---
+++ bot.py
@@ -0,0 +1,37 @@
+import argparse
+import logging
+
+from input_parser import InputParser as Input
+from pathlib import Path
+from twitch_bot import TwitchBot
+
+
+
+def parse_args():
+    argument_parser = argparse.ArgumentParser(description='Process some integers.')
+    argument_parser.add_argument('-l', '--logfile', type=Path, default=Path('Salamandbot2.log'),
+                        help="Path, with filename, to write the log to")
+
+    return argument_parser.parse_args()
+
+
+args = parse_args()
+
+file_log = logging.FileHandler(args.logfile, encoding='utf-8')
+logging.basicConfig(handlers=[file_log], level=logging.INFO,
+                    format="{asctime}:{levelname}:{name}:{message}", style="{")
+
+
+def commands():
+    command_link = "https://docs.google.com/document/d/1TLSf6pbiqqNrKha85TxqoJWIluovDWGaLId8_jTB4to/edit?usp=sharing"
+    print(f"Commands can be found here: {command_link}")
+
+if __name__ == "__main__":
+    parser = Input(logger=logging.getLogger())
+    parser.add_command("!commands", commands)
+
+    bots = [TwitchBot(parser)]
+
+    logging.info("")
+    for bot in bots:
+        bot.run()
---
+++ input_parser.py
@@ -0,0 +1,57 @@
+import logging
+
+class InputParser:
+
+    input_queue = []
+    commands = {}
+
+    def __init__(self, logger=None):
+        """
+        Listens for results fired from input sources
+        :param input_sources: The bots that are sending events to the input parser
+        """
+        self.logger = logger
+
+    def parse_input(self, source, to_parse=None):
+        """
+        Parses input from the source.
+        :param source:
+        :param to_parse:
+        :return:
+        """
+        content = ""
+
+        # Get the message content based on which
+        if source == "twitch":
+            content = to_parse.content.lower()
+        elif source == "discord":
+            content = to_parse.content.lower()
+        elif source == "youtube":
+            pass
+
+        first_word = content.split()[0]  # Gets the first word of the input to determine the command it should run
+        output = ""
+        if first_word in self.commands.keys():
+            output = f"{source}: {self.commands[first_word](content)}"
+        else:
+            logging.info(f"[Input Parser] {first_word} is not a valid command.")
+
+        return output
+
+    def add_command(self, command_name, command_func):
+        """
+        Adds a command to the command list.
+        :param command_name: The name of the command being added.
+        :param command_func: The function associated with the command.
+        :return: True if the command was successfully added, false if it already exists
+        """
+
+        if command_name not in self.commands.keys():
+            self.commands[command_name] = command_func
+            return True
+        else:
+            logging.info(f"[Input Parser]{command_name} is already a command in this current parser.")
+            return False
+
+    def save_commands(self):
+        pass
