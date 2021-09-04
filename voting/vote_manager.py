229fc7105d819c1caa0dd2449a34c56f1ec1c048
NewtC <newtc12@gmail.com>
2021-09-04 11:23:09 -0700

    Attempted fix for a nonetype return where there shouldn't be one.

--- voting/vote_manager.py
+++ voting/vote_manager.py
@@ -80,15 +80,18 @@ class VoteManager:
                             break
                         next_profile_change = times
 
+
                 if next_profile_change:
                     # get the original key again
                     schedule_index = f"{next_profile_change[3]}:{str(next_profile_change[4]).zfill(2)}"
                     # if the active profile is already the pending profile, then return none.
                     if hf.get_active_profile() == schedule[schedule_index]:
                         return None
-
-                schedule_index = f"{next_profile_change[3]}:{str(next_profile_change[4]).zfill(2)}"
-                return schedule_index
+                    else:
+                        schedule_index = f"{next_profile_change[3]}:{str(next_profile_change[4]).zfill(2)}"
+                        return schedule_index
+                else:
+                    return None
             else:
                 return None
 
