8a1dff390332333733f4bb8ef0c93fa1c74d6143
NewtC <newtc12@gmail.com>
2021-09-29 20:40:01 -0700

    Fixed the moonrise OBS script so that it automatically finds the moonrise_status file.

--- scripts/moonrise-icon-updater.py
+++ scripts/moonrise-icon-updater.py
@@ -1,8 +1,10 @@
 import obspython as obs
 import json
+import os
 
 interval = 5
 status_directory = ""
+started = False
 
 
 # ------------------------------------------------------------
@@ -21,7 +23,7 @@ def set_source_visibility(source_name: str, visibilty: bool):
 
 def update_icons():
     global status_directory
-
+    obs.timer_remove(update_icons)
     with open(status_directory, encoding="utf-8-sig", mode="r") as file_reader:
         status = json.load(file_reader)
 
@@ -30,6 +32,7 @@ def update_icons():
         set_source_visibility("Soil Kill", status["soil_kill"])
         set_source_visibility("Bjorn Ready", status["bjorn_ready"])
         set_source_visibility("Bjorn Splinter", status["bjorn_delay"])
+        obs.timer_add(update_icons, interval * 1000)
     except Exception as err:
         obs.script_log(obs.LOG_WARNING, err)
 
@@ -38,27 +41,31 @@ def refresh_pressed(props, prop):
     update_icons()
 
 
+def start_pressed(props, prop):
+    global started
+    started = not started
+    obs.script_log(obs.LOG_INFO, f"Settings started to: {started}")
+
+    if started:
+        obs.timer_add(update_icons, interval * 1000)
+    else:
+        obs.timer_remove(update_icons)
+
+
 # ------------------------------------------------------------
 
 def script_description():
     return "Updates icons required for the Moonrise stream event.\n\nBy NewtC"
 
 
-def script_update(settings):
-    global interval
-    global status_directory
-
-    interval = obs.obs_data_get_int(settings, "interval")
-    status_directory = obs.obs_data_get_string(settings, "status_directory")
-    obs.timer_remove(update_icons)
-
-    if status_directory and interval:
-        obs.timer_add(update_icons, interval * 1000)
-
-
 def script_defaults(settings):
+    global status_directory
     obs.obs_data_set_default_int(settings, "interval", 30)
-    obs.obs_data_set_string(settings, "status_directory", "..\\values\\moonrise_status.json")
+    path_to_script_dir = "\\".join(os.path.realpath(__file__).split("\\")[0:-1])
+    default_dir = os.path.join(path_to_script_dir, "..\\values\\moonrise_status.json")
+    status_directory = default_dir
+    obs.script_log(obs.LOG_INFO, f"Current working directory: {default_dir}")
+    obs.obs_data_set_string(settings, "status_directory", default_dir)
 
 
 def script_properties():
@@ -67,5 +74,6 @@ def script_properties():
     obs.obs_properties_add_text(props, "status_directory", "Status Directory", obs.OBS_TEXT_DEFAULT)
     obs.obs_properties_add_int(props, "interval", "Update Interval (seconds)", 5, 3600, 1)
 
-    obs.obs_properties_add_button(props, "button", "Refresh:", refresh_pressed)
+    obs.obs_properties_add_button(props, "button", "Start\\Stop", start_pressed)
+    obs.obs_properties_add_button(props, "button", "Refresh", refresh_pressed)
     return props
